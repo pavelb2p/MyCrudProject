f339ad31bcb581f97fb790ff4b38b410a59cb871
pavelb2p <p.mistsenko@gmail.com>
2022-03-18 12:31:51 +0200

    My first commit

---
+++ src/main/java/com/example/mycrudproject/MyCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycrudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCrudProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCrudProjectApplication.class, args);
+    }
+
+}
